740a54517b97121441ba3ae91755bfa2ab96772e
Devorient <sam.hylamia@it.uu.se>
2020-01-16 00:51:10 +0100

    creating a wsgi entry

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from app.helpers import verify_password
-application = Flask(__name__, instance_relative_config=True)
-application.config.from_object('config')
-application.config.from_object('config_local')
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_object('config_local')
 
 from nexa_controller_rpi import switcher
 
-from application import views
+from app import views
