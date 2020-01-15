63a1dcdb5bdc2a58a87642ca62b4bc3109d1e498
Devorient <sam.hylamia@it.uu.se>
2020-01-15 20:06:35 +0100

    adapt to new implementation

--- nexa_controller_rpi/manual_switch.py
+++ nexa_controller_rpi/manual_switch.py
@@ -3,10 +3,10 @@ import sys
 from switch_nexa import NexaSwitcher
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 3:
         sys.exit('Usage: %s on|off data_pin_number transmitter_code' % sys.argv[0])
 
-    switcher = NexaSwitcher(data_pin=int(sys.argv[2]), transmitter_code=int(sys.argv[3]))
+    switcher = NexaSwitcher(16, transmitter_code=int(sys.argv[2]))
     if sys.argv[1] == "on":
         switcher.switch(True)
     else:
